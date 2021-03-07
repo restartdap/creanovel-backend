5a1f5b74855faddf4ff4ebf698f208d5a5d1da56
Rafael Estrada <restradap1600@gmail.com>
2021-03-06 22:09:21 -0600

    add database connection to server

--- src/server.js
+++ src/server.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const dbConnection = require('./database/database');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.novelasPath = "/api/novelas";
+
+        this.datadase();
 
         this.middlewares();
         this.routes();
     }
 
+    async datadase() {
+        await dbConnection();
+    }
+
     middlewares() {
-        this.app.use(cors());
+        // this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.static('src/public'));
     }
 
     routes() {
-        this.app.get("/", (req, res) => {
-            res.status(200).json({
-                message: "Hello World"
-            });
-        });
+        this.app.use(this.novelasPath, require('./routes/novela.route'));
     }
 
     listen() {
