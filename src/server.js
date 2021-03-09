6ba829e72eea01fc83ab141557fe6330d8b79d39
Rafael Estrada <restradap1600@gmail.com>
2021-03-09 13:15:36 -0600

    add Escena routes to server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.novelasPath = "/api/novelas";
+        this.escenasPath = "/api/escenas";
 
         this.datadase();
 
@@ -26,6 +27,7 @@ class Server {
 
     routes() {
         this.app.use(this.novelasPath, require('./routes/novela.route'));
+        this.app.use(this.escenasPath, require('./routes/escena.route'));
     }
 
     listen() {
