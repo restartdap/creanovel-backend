5d70a87a53e151bae221a0cdafeaf7b2f8e95859
Rafael Estrada <restradap1600@gmail.com>
2021-03-09 10:41:44 -0600

    add Recurso reference in Conversacion model

--- src/models/conversacion.model.js
+++ src/models/conversacion.model.js
@@ -1,6 +1,11 @@
-const { model } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const conversacionSchema = new Schema({
+    recurso: {
+        type: Schema.Types.ObjectId,
+        ref: "Recurso"
+    },
     texto: {
         type: String,
         required: true,
