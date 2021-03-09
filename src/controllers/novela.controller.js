a6167eebc9c3efc23bd46a1adb877101e269042c
Rafael Estrada <restradap1600@gmail.com>
2021-03-09 13:11:21 -0600

    update Novela controller

--- src/controllers/novela.controller.js
+++ src/controllers/novela.controller.js
@@ -1,65 +1,59 @@
 const { request, response } = require('express');
-const Novela = require('../models/novela.model');
-const { primeraLetraMayuscula } = require("../helpers/helpers");
+const novelaServices = require('../services/novela.services');
 
-const getNovela = (req = request, res = response) => {
-    const id = req.params.id;
-
-    Novela.findById(id, (err, dbNovela) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
-
+const getNovela = async (req = request, res = response) => {
+    try {
+        const id = req.params.id.toString();
+        const novela = await novelaServices.findById(id);
         return res.json({
             ok: true,
-            novela: dbNovela
+            novela
         });
-    });
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            error
+        });
+    }
 };
 
-const getNovelas = (req = request, res = response) => {
-    Novela.find().exec((err, novelas) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
-
+const getNovelas = async (req = request, res = response) => {
+    try {
+        const novelas = await novelaServices.getAllNovelasWithEscenas();
         return res.json({
             ok: true,
             novelas
         });
-    });
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            error
+        });
+    }
 };
 
-const postNovela = (req = request, res = response) => {
-    const body = req.body;
-    const titulo = primeraLetraMayuscula(body.titulo.toLowerCase());
-    const { descripcion, estado } = body;
-
-    const novela = new Novela({
-        titulo,
-        descripcion,
-        estado
-    });
-
-    novela.save((err, dbNovela) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
+const postNovela = async (req = request, res = response) => {
+    try {
+        const { titulo, descripcion, estado } = req.body;
+        const escenas = [];
+        const dbNovela = await novelaServices.createNovela({
+            titulo,
+            descripcion,
+            estado,
+            escenas
+        });
 
         return res.json({
             ok: true,
             novela: dbNovela
         });
-    });
+
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            error
+        });
+    }
 };
 
 module.exports = {
